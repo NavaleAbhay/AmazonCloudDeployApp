833c5eed5115982011f176330736a071bfd0bbde
Vinayak Bardale <vinayakbardale@gmail.com>
2025-06-06 20:52:03 +0530

    mvc app

---
+++ MyCloudReadyApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyCloudReadyApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
